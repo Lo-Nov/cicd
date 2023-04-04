3121f230d07662d60b0aae2b8e7a3792e03d6280
Loman <cheruiyot.edwin@eclectics.io>
2023-04-04 22:45:02 +0300

    first commit

---
+++ src/main/java/io/eclectics/cicd/CicdApplication.java
@@ -0,0 +1,20 @@
+package io.eclectics.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	@GetMapping
+	public String message(){
+		return "welcome to my first jenkins cicd test";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
